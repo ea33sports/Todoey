c577c72cf9e8ecd9fcf4601afc863ace0a810a54
Eric Developer <ea33sports@me.com>
2018-04-05 18:41:38 -0500

    Converto to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Eric Andersen on 4/5/18.
+//  Copyright © 2018 Eric Andersen. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done: Bool = false
+}
